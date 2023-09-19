6fe1d2415c1fc1900b651b3a294c8a582d099fe5
bonaxl015 <bonaxl015@gmail.com>
2023-09-20 00:20:39 +0800

    feat: install and setup packages

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,35 @@
 module.exports = function(api) {
-  api.cache(true);
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src'
+          },
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.json'
+          ]
+        }
+      ],
+      [
+        'react-native-reanimated/plugin',
+        {
+          relativeSourceLocation: true
+        }
+      ]
+    ]
+  }
+}
---
+++ index.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="./node_modules/nativewind/types.d.ts" />
+
+declare module '*.jpg'
+declare module '*.png'
+declare module '*.ts'
+declare module '*.tsx'

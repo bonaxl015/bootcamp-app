221a9fe096364fbaa490dc8e99e9051bd43e6f38
bonaxl015 <bonaxl015@gmail.com>
2023-09-30 01:31:21 +0800

    feat: debug border style

---
+++ src/styles/debug/border.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+const style = StyleSheet.create({
+  debugBorder: {
+    borderStyle: 'solid',
+    borderColor: 'red',
+    borderWidth: 1
+  }
+})
+
+export default style
